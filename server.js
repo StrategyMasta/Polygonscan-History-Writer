1f36e085bf4905d983329cea1b9fe1ff9881a746
StrategyMasta <matematyk_nowej_ery@tlen.pl>
2023-03-07 16:06:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -25,11 +25,11 @@ const server = app.listen(3000, async () => {
 
 
     // The User Chooses How He'd Like To Format The Data
-    if((process.env.OPTION).trim() == "observe") display(results);
-    else if((process.env.OPTION).trim() == "csv") displayCSV(results);
+    if((process.env.OPTION + "").trim() == "observe") display(results);
+    else if((process.env.OPTION + "").trim() == "csv") displayCSV(results);
 
     // The CSV Option Also Closes The Server
-    if((process.env.OPTION).trim() == "csv") server.close();
+    if((process.env.OPTION + "").trim() == "csv") server.close();
 });
 
 app.use(express.json());
